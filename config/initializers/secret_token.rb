98398c48a095d71342b7a54d3532de58d019eed3
Josh44 <just2000joshing@hotmail.com>
2013-11-14 12:33:14 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beep::Application.config.secret_key_base = '6ae6d461b27a5c970c782059b7533757fe8b5764939a7710cbbe8a3c3fdf57529e8c868fac14c11ab6a6b60b61a1998c4e89252dfdee4c02936b3cbf74d13473'
